47bcad5495a69dd939031cca013131007c80bfb6
Mohamed Samsudeen <samsudeenks@gmail.com>
2023-05-26 20:04:55 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Counter: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
